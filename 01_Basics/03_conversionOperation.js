96ef7d03dfa83e549c7ac54a2b78fc7df68937e5
Kumkum Kumari <kumkum981@gmail.com>
2024-12-19 19:20:05 +0000

    camparision

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -67,8 +67,66 @@
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ****************************** //
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(3/2);
+// console.log(7%2);
+
+// let str1 = "Meter"
+// let str2 = " Down"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(3 + 4 * 5 % 3);
+// console.log((3 + 4) * 5 % 3);
+// console.log((3 + 4) * (5 % 3));
+// console.log(3 + (4 * 5 % 3));
+// console.log(3 + (4 * 5) % 3);
+
+// console.log(true); // no need to use
+// console.log(+true); // no need to use
+// console.log(+""); // no need to use
+// console.log(+false); // no need to use
+
+// let num1, num2, num3; // no need to use
+// num1 = num2 = num3 = 2 + 2 // no need to use
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+// prefix and postfix increment
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
