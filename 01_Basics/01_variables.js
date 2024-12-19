85fbb55dbed7d680993f5bda02715c429868955d
Kumkum Kumari <kumkum981@gmail.com>
2024-12-19 15:26:39 +0000

    datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144355
+let accountEmail = "kumkum@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+// accountId = 2
+
+accountEmail="kk@gmail.com"
+accountPassword="212121"
+accountCity="Mount Abu"
+let accountState;
+
+//console.log(accountPassword);
+//console.table([accountId, accountEmail, accountPassword, accountCity]);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all JS code as a newer version
+
+// alert("okay") // We are using node.js, not browser
+
+console.log(3
+    +
+    3); // Code readability should be high.
+
+console.log("Kumkum");
+
+let name = "Kumkum"
+let age = 19
+let isLoggedIn = true
+let state;
+
+// Primitive Datatypes:
+// Number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => declared the value but not defined
+// symbol => unique
+
+// Non-Primitive Datatypes:
+// Object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+
+
+
