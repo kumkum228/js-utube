5600f7bd3f058b19fa45c3fb1290d1a6cc6cece1
Kumkum Kumari <kumkum981@gmail.com>
2024-12-19 15:59:00 +0000

    conversion

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,76 @@
+// let score = 33
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score = "Kumkum"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+// let isLoogedIn = 1
+// let booleanIsLoogedIn = Boolean(isLoogedIn)
+// console.log(booleanIsLoogedIn);
+// console.log(typeof booleanIsLoogedIn);
+
+// let isLoogedIn = 0
+// let booleanIsLoogedIn = Boolean(isLoogedIn)
+// console.log(booleanIsLoogedIn);
+// console.log(typeof booleanIsLoogedIn);
+
+// let isLoogedIn = ""
+// let booleanIsLoogedIn = Boolean(isLoogedIn)
+// console.log(booleanIsLoogedIn);
+// console.log(typeof booleanIsLoogedIn);
+
+// let isLoogedIn = "Rani"
+// let booleanIsLoogedIn = Boolean(isLoogedIn)
+// console.log(booleanIsLoogedIn);
+// console.log(typeof booleanIsLoogedIn); 
+
+// 1 => true; 0 => false
+// "" => false; "rani" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
