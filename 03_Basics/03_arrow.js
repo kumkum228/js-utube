88ab57dcf221e4708e64034f95b475d8757c967c
Kumkum Kumari <kumkum981@gmail.com>
2024-12-23 15:32:19 +0000

    arrays

---
+++ 03_Basics/03_arrow.js
@@ -0,0 +1,143 @@
+// const user = {
+//     username: "kumkum",
+//     price: 999,
+
+//     welcomeMessage: function() {
+//         console.log(`${this.username} , welcome to website`);        
+//     }
+// }
+// user.welcomeMessage()
+
+// const user = {
+//     username: "kumkum",
+//     price: 999,
+
+//     welcomeMessage: function() {
+//         console.log(`${this.username} , welcome to website`);        
+//     }
+// }
+// user.welcomeMessage()
+// user.username = "Rani"
+// user.welcomeMessage()
+
+// const user = {
+//     username: "kumkum",
+//     price: 999,
+
+//     welcomeMessage: function() {
+//         console.log(`${this.username} , welcome to website`);        
+//         console.log(this);
+//     }
+// }
+// user.welcomeMessage()
+// user.username = "Rani"
+// user.welcomeMessage()
+
+// // console.log(this);
+
+// const user = {
+//     username: "kumkum",
+//     price: 999,
+//     welcomeMessage: function() {
+//         console.log(`${this.username} , welcome to website`);        
+//         console.log(this);
+//     }
+// }
+// // user.welcomeMessage()
+// // user.username = "Rani"
+// // user.welcomeMessage()
+
+// console.log(this);
+
+// const user = {
+//     username: "kumkum",
+//     price: 999,
+//     welcomeMessage: function() {
+//         console.log(`${this.username} , welcome to website`);        
+//         console.log(this);
+//     }
+// }
+// user.welcomeMessage()
+// user.username = "Rani"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// it returns all the global values
+// function one() {
+//     console.log(this); 
+// }
+// one() 
+
+// this is not working under the function
+// return undefined
+// function one() {
+//     let username = "Kumkum"
+//     console.log(this); 
+// }
+// one()
+ 
+// return undefined
+// const chai = function () {
+//     let username = "kumkum"
+//     console.log(this.username);
+// }
+// chai()
+
+// return undefined
+// const chai = ()  => {
+//     let username = "kumkum"
+//     console.log(this.username);
+// }
+// chai()
+
+// return undefined
+// const chai = ()  => {
+//     let username = "kumkum"
+//     console.log(this);
+// }
+// chai()
+
+// Explicit return
+// add return in curly braces 
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+// console.log(addTwo(3, 4));
+
+
+// const addTwo = (n1, n2) => n1 + n2
+// console.log(addTwo(3, 4));
+
+// implicit return- Need to use return in parenthesis()
+// const addTwo = (n1, n2) => (n1 + n2)
+// console.log(addTwo(3, 4));
+
+// return object
+// const addTwo = (n1, n2) => ({username: "kk"})
+// console.log(addTwo(3, 4));
+
+// const myArray = [2, 5, 3, 7, 8]
+// myArray.forEach()
+
+// const myArray = [2, 5, 3, 7, 8]
+// myArray.forEach(function () {})
+
+// const myArray = [2, 5, 3, 7, 8]
+// myArray.forEach(() => {})
+
+// const myArray = [2, 5, 3, 7, 8]
+// myArray.forEach(() => ())
+
+
+
+
+
+
+
+
+
+
+
+
+
