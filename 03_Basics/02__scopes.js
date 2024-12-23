0b83a3f6c6b85a45eb6fd1c2dcaa5f6ee5721b6e
Kumkum Kumari <kumkum981@gmail.com>
2024-12-23 13:46:07 +0000

    scopes

---
+++ 03_Basics/02__scopes.js
@@ -0,0 +1,22 @@
+// Nested Scopes
+
+// function one() {
+//     const username="Kumkum"
+//     function two() {
+//         const website="youtube"
+//         console.log(username);        
+//     }
+// //  console.log(website);
+//     two()
+// }
+// one()
+
+if (true) {
+    const username="kumkum"
+    if (username==="kumkum") {
+        const website=" youtube"
+        console.log(username + website);        
+    }
+    // console.log(website);  // 1st Error   
+}
+// console.log(username); // 2nd Error
