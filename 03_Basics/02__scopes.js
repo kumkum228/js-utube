917b2d0815c6c03c4e9d35bf41756a8d6944ec13
Kumkum Kumari <kumkum981@gmail.com>
2024-12-23 13:58:02 +0000

    nested

--- 03_Basics/02__scopes.js
+++ 03_Basics/02__scopes.js
@@ -11,12 +11,45 @@
 // }
 // one()
 
-if (true) {
-    const username="kumkum"
-    if (username==="kumkum") {
-        const website=" youtube"
-        console.log(username + website);        
-    }
+// if (true) {
+//     const username="kumkum"
+//     if (username==="kumkum") {
+//         const website=" youtube"
+//         console.log(username + website);        
+    // }
     // console.log(website);  // 1st Error   
-}
+// }
 // console.log(username); // 2nd Error
+
+// function addone(num) {
+//     return num + 1
+// }
+// console.log(addone(5));
+
+
+// const addTwo = function(num) {
+//     return num + 2
+// }
+// console.log(addTwo(5));
+
+// console.log(addone(5));
+// function addone(num) {
+//     return num + 1
+// }
+
+// const addTwo = function(num) {
+//     return num + 2
+// }
+// console.log(addTwo(5));
+
+// console.log(addone(5));
+// function addone(num) {
+//     return num + 1
+// }
+
+// console.log(addTwo(5));
+// const addTwo = function(num) {
+//     return num + 2
+// } // value not return
+
+
