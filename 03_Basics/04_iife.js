feab312a6c1e63db01c6bb346c223ac6d86f1c96
Kumkum Kumari <kumkum981@gmail.com>
2024-12-23 15:56:31 +0000

    iife

---
+++ 03_Basics/04_iife.js
@@ -0,0 +1,41 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+// function chai() {
+//     console.log(`Database connected`);   
+// }
+// chai()
+// console.log(chai());
+
+// (function chai() {
+//     console.log(`Database connected`);   
+// })()
+
+// (define function)(execution) - ()()
+
+
+// ( function coffee() {
+//     console.log(` Hot Coffee `);
+// } )();
+
+// ( () =>  {
+//     console.log(` Cold Coffee `);
+// } )()
+
+// ( function coffee() {
+//     console.log(` Hot Coffee `);
+// } )();
+
+// ( function tea() {
+//     console.log(` Hot Tea `);
+// } )()
+
+// ( function coffee() {
+// coffee() - named IIFE
+//     console.log(` Hot Coffee `);
+// } )();
+
+// ( (name) => {
+// (name) - unnamed IIFE
+//     console.log(` Coffee is ready for ${name} `);
+// } )('kumkum')
+
