9e114891d750d2fb5ac158e261a5a338feb72045
Kumkum Kumari <kumkum981@gmail.com>
2024-12-24 14:04:35 +0000

    one

---
+++ 04_control_flow/one.js
@@ -0,0 +1,115 @@
+// if
+// condition is always true. if not then not excuted code within scope
+
+// if (condition) { 
+    
+// }
+
+// if (2 == "2") {
+//     console.log("executed");   
+// }
+
+// if (2 != 3) {
+//     console.log("executed");    
+// }
+
+// const isUserloggedIn = true
+// if (isUserloggedIn) {
+    
+// }
+
+// const isUserloggedIn = true
+// const temperature = 41
+
+// if ( temperature === 50 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// const isUserloggedIn = true
+// const temperature = 41
+
+// if ( temperature === 41 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// const isUserloggedIn = true
+// const temperature = 41
+
+// if ( temperature === 41 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+// console.log("execute");
+
+
+// const isUserloggedIn = true
+// const temperature = 41
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+// console.log(`User power: ${power}`);
+
+
+// const balance = 1000
+// if (balance > 500) console.log("test"),console.log("test2");
+
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+
+// const userLoggedIn = true
+// const debitCard = true
+
+// if (userLoggedIn && debitCard) {
+//     console.log("Allow to buy course");
+// }
+
+// const userLoggedIn = true
+// const debitCard = true
+
+// if (userLoggedIn && debitCard && 2==2) {
+//     console.log("Allow to buy course");
+// }
+
+
+// const userLoggedIn = true
+// const debitCard = true
+// const loggedInFromGoogle = false
+// const loggedInFromEmail = true
+
+// if (userLoggedIn && debitCard && 2==3) {
+//     console.log("Allow to buy course");
+// }
+
+// if (loggedInFromGoogle || loggedInFromEmail) {
+//     console.log("User logged in");
+// }
