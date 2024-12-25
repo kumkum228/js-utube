2713419475b2a2ce587ca07a2cb1ea3992cb6257
Kumkum Kumari <kumkum981@gmail.com>
2024-12-25 12:09:12 +0000

    loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,63 @@
+// while or do while loop
+// while loop
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2;  
+// }
+
+// let index = 1
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2;  
+// }
+
+// let index = 2
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2;  
+// }
+
+// let index = 4
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2;  
+// }
+
+// let index = 6
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2;  
+// }
+
+// let index = 6
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2;  
+// }
+
+// let myArray = ['flash', 'batman', 'aryaman']
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1;        
+// }
+
+// do while loop
+
+// let score = 1
+// do {
+//     console.log(`Score is ${score}`);
+//     score++;
+// } while (score <= 10);
+
+// let score = 11
+// do {
+//     console.log(`Score is ${score}`);
+//     score++;
+// } while (score <= 10);
+
+
+
+
