54c6b83ff90e864ec82a717c905dc4216c81a5fd
Kumkum Kumari <kumkum981@gmail.com>
2024-12-25 12:46:05 +0000

    for of loop

---
+++ 05_iterations/three.js
@@ -0,0 +1,81 @@
+// for of loop
+// Syntax:
+// for (const element of object) {}
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+// const arr = [1, 2, 3, 4, 5]
+// for (const num of arr) {
+//     console.log(num);
+    
+// }
+
+// const arr = ["Haryana", "Keral", "Odisa", "Bihar", "Delhi"]
+// for (const state of arr) {
+//     console.log(state);  
+// }
+
+// const greetings = "Hello world!"
+// for (const Quote of greetings) {
+//     console.log(`Each char is ${Quote}`);    
+// }
+
+// const greetings = "Hello world!"
+// for (const Quote of greetings) {
+//     console.log(`Each char is ${Quote}`);   
+// }
+
+// Maps - print unique values in sequence.
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('US', "United States")
+// map.set('UK', "United Kingdom")
+// console.log(map);
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('US', "United States")
+// map.set('UK', "United Kingdom")
+// map.set('IN', "India")
+
+// console.log(map);
+
+
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('US', "United States")
+// map.set('UK', "United Kingdom")
+
+// for (const key of map) {
+//     console.log(key);   
+// }
+
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('US', "United States")
+// map.set('UK', "United Kingdom")
+
+// for (const [key, value] of map) {
+//     console.log(key, ':-', value);   
+// }
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('US', "United States")
+// map.set('UK', "United Kingdom")
+
+// for (const [key, value] of map) {
+//     console.log(key, ':-', value);   
+// }
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+// for (const [key, value] of myObject) {
+//         console.log(key, ':-', value);       
+// }
