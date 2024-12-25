72240634d20ebeac4b1b9159745f5734164d72e0
Kumkum Kumari <kumkum981@gmail.com>
2024-12-25 15:00:20 +0000

    forEach loop

---
+++ 05_iterations/five.js
@@ -0,0 +1,54 @@
+// forEach
+
+// const coding = ["JS", "Ruby", "Java", "Python", "CPP"]
+// coding.forEach(function (item) {})
+
+// const coding = ["JS", "Ruby", "Java", "Python", "CPP"]
+// coding.forEach(function greet() {})
+
+// const coding = ["JS", "Ruby", "Java", "Python", "CPP"]
+// coding.forEach(function (val) {
+//     console.log(val);   
+// })
+
+// const coding = ["JS", "Ruby", "Java", "Python", "CPP"]
+// coding.forEach( (val) => {
+//     console.log(val);   
+// } ) 
+
+// const coding = ["JS", "Ruby", "Java", "Python", "CPP"]
+// function printMe(item) {
+//     console.log(item);        
+// }
+// coding.forEach(printMe)
+
+
+// const coding = ["JS", "Ruby", "Java", "Python", "CPP"]
+// coding.forEach( (item) => {
+//     console.log(item);    
+// } )
+
+// const coding = ["JS", "Ruby", "Java", "Python", "CPP"]
+// coding.forEach( (item, index, arr) => {
+//     console.log(item, index, arr);    
+// } )
+
+// const myCoding = [
+//     {
+//         languageName: "JavaScript",
+//         languageFileName: "JS"
+//     },
+//     {
+//         languageName: "Ruby",
+//         languageFileName: "RB"
+//     },
+//     {
+//         languageName: "Python",
+//         languageFileName: "Py"
+//     }
+// ]
+// myCoding.forEach( (item) => {
+//     console.log(item.languageName);   
+// } )
+
+
