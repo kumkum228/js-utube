ee97d7a15a2b7a88f571ccbcea4a2f932b14d3e5
kumkum228 <kumkum981@gmail.com>
2025-01-07 14:22:32 +0000

    math pi

--- 10_Classes_and_OOP/mathpi.js
+++ 10_Classes_and_OOP/mathpi.js
@@ -18,14 +18,14 @@ const chai = {
 console.log(Object.getOwnPropertyDescriptor(chai, "name"));
 
 Object.defineProperty(chai, 'name', {
-    //writable: false,
+    // writable: false,
     enumerable: true,  
 })
 
-// console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
 
-// for (let [key, value] of Object.entries(chai)) {
-//     if (typeof value !== 'function') {        
-//         console.log(`${key} : ${value}`);
-//     }
-// }
+for (let [key, value] of Object.entries(chai)) {
+    if (typeof value !== 'function') {        
+        console.log(`${key} : ${value}`);
+    }
+}
