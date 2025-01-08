932ccddbea0b5c773832051e79c12d0dcc1668eb
kumkum228 <kumkum981@gmail.com>
2025-01-08 14:19:57 +0000

    defined object on get and set

--- 10_Classes_and_OOP/object_get_set.js
+++ 10_Classes_and_OOP/object_get_set.js
@@ -1,2 +1,37 @@
 // Object based syntax(get and set)
+// Ex1: 
 
+// const User = {
+//     _email: 'kk@kyc.com',
+//     _password: "abc123",
+
+//     get email(){
+//         return this._email.toUpperCase()
+//     },
+
+//     set password(value){
+//         this._email = value
+//     }
+// }
+
+// // Factory Function used below: universal object
+// const tea = Object.create(User)
+// console.log(tea.email);
+
+// Ex2:
+const User = {
+    _email: 'kk@kyc.com',
+    _password: "abc123",
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+
+    set email(value){
+        this._email = value
+    }
+}
+
+// Factory Function used below: universal object
+const tea = Object.create(User)
+console.log(tea.email);
